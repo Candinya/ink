d9c055f9568f431635a2d18c82c593a89e098380
Nya Candy <dev@candinya.com>
2024-12-04 21:54:17 +0800

    chore: tweak SkillDeveloper timing
    since no need to hide projects, we can have a little more time for tech stack

--- src/sections/About/Skills/Developer/index.tsx
+++ src/sections/About/Skills/Developer/index.tsx
@@ -19,37 +19,34 @@ const SkillDeveloper = () => {
 
   const techStackTransform = useTransform(
     scrollYProgress,
-    [0.25, 0.3, 0.6, 0.65],
+    [0.25, 0.3, 0.65, 0.7],
     ["0fr", "1fr", "1fr", "0fr"],
   );
   const techStackFrontendTransform = useTransform(
     scrollYProgress,
-    [0.3, 0.4, 0.5, 0.6],
+    [0.3, 0.4, 0.55, 0.65],
     [0, 1, 1, 0],
   );
   const techStackBackendTransform = useTransform(
     scrollYProgress,
-    [0.4, 0.5, 0.6],
-    [0, 1, 0],
+    [0.4, 0.5, 0.55, 0.65],
+    [0, 1, 1, 0],
   );
 
   const projectsTransform = useTransform(
     scrollYProgress,
-    [0.6, 0.65, 0.95, 1],
-    // ["0fr", "1fr", "1fr", "0fr"], // 这个配置模式下最后标题回回到中间
-    ["0fr", "1fr", "1fr", "1fr"], // 这个配置模式下最后标题不会回到中间
+    [0.65, 0.7],
+    ["0fr", "1fr"],
   );
   const projectsOwnTransform = useTransform(
     scrollYProgress,
-    [0.65, 0.75, 0.85, 0.95],
-    // [0, 1, 1, 0], // 这个配置模式下最后会淡出
-    [0, 1, 1, 1], // 这个配置模式下最后不会淡出
+    [0.7, 0.8],
+    [0, 1],
   );
   const projectsContributeTransform = useTransform(
     scrollYProgress,
-    [0.75, 0.85, 0.95],
-    // [0, 1, 0], // 同上
-    [0, 1, 1],
+    [0.8, 0.9],
+    [0, 1],
   );
 
   return (
