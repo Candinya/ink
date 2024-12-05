4da961f9676c70bf8e5c2d94d53208eeb1a1db6f
Nya Candy <dev@candinya.com>
2024-12-05 22:58:39 +0800

    chore: optimize code

--- src/sections/About/index.tsx
+++ src/sections/About/index.tsx
@@ -3,15 +3,13 @@ import Name from "./Name";
 import Skills from "./Skills";
 import More from "./More";
 
-const About = () => {
-  return (
-    <div className="-mt-screen/4 w-full flex flex-col justify-center items-center">
-      <SectionTitle title={"关于我"} />
-      <Name />
-      <Skills />
-      <More />
-    </div>
-  );
-};
+const About = () => (
+  <div className="-mt-screen/4 w-full flex flex-col justify-center items-center">
+    <SectionTitle title={"关于我"} />
+    <Name />
+    <Skills />
+    <More />
+  </div>
+);
 
 export default About;
