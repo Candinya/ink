01305eba8f6d33e6a4574a0fcdb2d9570afdae99
Nya Candy <dev@candinya.com>
2024-12-05 19:49:43 +0800

    chore: tweak image style

--- src/sections/About/Skills/DevOps/Projects/LocalImage.tsx
+++ src/sections/About/Skills/DevOps/Projects/LocalImage.tsx
@@ -1,91 +1,52 @@
 "use client";
 
-import { motion } from "motion/react";
-import { useState } from "react";
 import Image, { StaticImageData } from "next/image";
-import {
-  Dialog,
-  DialogPanel,
-  DialogTitle,
-  DialogBackdrop,
-} from "@headlessui/react";
 
 interface LocalImageProps {
-  id: string;
-  className: string;
+  containerClassName: string;
+  contentClassName: string;
   src: StaticImageData;
   alt: string;
   width: number;
   height: number;
 }
 const LocalImage = ({
-  id,
-  className,
+  containerClassName,
+  contentClassName,
   src,
   alt,
   width,
   height,
-}: LocalImageProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <>
-      {/*图片本身*/}
-      <motion.button
-        className={`w-full h-full rounded overflow-clip group ${className}`}
-        // layoutId={`about-skills-devops-projects-local-image-${id}-container`}
-        onClick={() => {
-          setIsOpen(true);
-        }}
-      >
-        <div className="w-full h-full flex justify-center items-center">
-          <motion.div
-            className="w-full"
-            layoutId={`about-skills-devops-projects-local-image-${id}-image`}
-          >
-            <Image
-              className="w-full h-full object-contain group-hover:scale-110 transition-transform duration-500"
-              src={src}
-              alt={alt}
-              width={width}
-              height={height}
-            />
-          </motion.div>
-        </div>
-      </motion.button>
-
-      {/*点击后展开全屏展示的模态框*/}
-      <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-        <DialogBackdrop
-          transition
-          className="fixed inset-0 bg-black bg-opacity-30 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+}: LocalImageProps) => (
+  <div
+    className={`w-full h-full rounded hover:overflow-visible hover:z-10 group relative ${containerClassName}`}
+  >
+    <div
+      className={`w-full h-full flex justify-center items-center overflow-clip ${contentClassName}`}
+    >
+      <div className="w-full">
+        <Image
+          className="w-full h-full object-contain"
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
         />
+      </div>
+    </div>
 
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex w-full min-h-full justify-center text-center items-center p-16">
-            <DialogPanel
-              transition
-              className="relative transform overflow-hidden transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
-            >
-              <motion.div
-                className="w-full"
-                layoutId={`about-skills-devops-projects-local-image-${id}-image`}
-              >
-                <Image
-                  className="w-full object-contain rounded-3xl"
-                  src={src}
-                  alt={alt}
-                  width={width}
-                  height={height}
-                  fill={false}
-                />
-              </motion.div>
-            </DialogPanel>
-          </div>
-        </div>
-      </Dialog>
-    </>
-  );
-};
+    <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center overflow-visible opacity-0 group-hover:opacity-100 transition-opacity duration-500 pointer-events-none">
+      <div className="w-full">
+        <Image
+          className="w-full h-full object-contain rounded-3xl"
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+        />
+      </div>
+    </div>
+  </div>
+);
 
 export default LocalImage;
