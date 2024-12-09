cf873b6286466dd7502aa75aa83a19f8ddea1b3a
Nya Candy <dev@candinya.com>
2024-12-09 09:54:23 +0800

    chore: simplify color

--- src/sections/World/Activities/ActivityCount/HeatMap/colors.ts
+++ src/sections/World/Activities/ActivityCount/HeatMap/colors.ts
@@ -1,6 +1,4 @@
 export const colorMapGithub = [
-  "bg-green-100",
-  "bg-green-200",
   "bg-green-300",
   "bg-green-400",
   "bg-green-500",
@@ -8,15 +6,14 @@ export const colorMapGithub = [
 ];
 
 export const colorMapMisskey = [
-  "bg-blue-100",
-  "bg-blue-200",
   "bg-blue-300",
   "bg-blue-400",
   "bg-blue-500",
   "bg-blue-600",
 ];
 
-const log100 = Math.log(100);
-
-export const colorMappingCalc = (val: number, max: number, into: number) =>
-  Math.floor((Math.log((val / max) * 100 + 1) / log100) * into); // 使用对数函数压小一点差距，避免浓的太浓淡的太淡
+export const colorMappingCalc = (
+  val: number,
+  max: number,
+  colorsCount: number,
+) => Math.floor((val / max) * colorsCount);
