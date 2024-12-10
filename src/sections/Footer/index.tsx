07bbca1600616cab1a238426f1bb3044a9a6a89e
Nya Candy <dev@candinya.com>
2024-12-10 14:28:38 +0800

    chore: tweak footer style

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -5,11 +5,11 @@ import FooterImage from "./assets/image.webp";
 
 const Footer = () => (
   <div className="w-full mt-12">
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center -mb-12">
       <img className="w-48" src={FooterImage} alt="喵" />
     </div>
 
-    <div className="w-full bg-neutral-800 text-white">
+    <div className="w-full bg-neutral-800 text-white pt-12">
       <div className="w-full mx-auto max-w-7xl px-12 py-6">
         <div className="flex flex-col justify-center items-center lg:flex-row gap-2 lg:gap-4">
           <span>&copy; {new Date().getUTCFullYear()} Nya Candy 版权所有</span>
